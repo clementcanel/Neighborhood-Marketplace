ee4e89540345dbc6a9ecfdb21c335da3655080b8
Mohamed Mohamedali <momo4179@colorado.edu>
2023-11-09 18:44:53 -0700

    unit test minor fix

--- Code/src/test/server.spec.js
+++ Code/src/test/server.spec.js
@@ -32,7 +32,7 @@ it('positive : /login', done => {
     chai
       .request(server)
       .post('/login')
-      .send({username: 'John Doe', email: 'testEmail@gmail.com', rating: 5.0, verified: TRUE})
+      .send({username: 'John Doe', email: 'testEmail@gmail.com', rating: 5.0, verified: true})
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.message).to.equals('Success');
@@ -46,7 +46,7 @@ it('Negative : /login. Checking invalid name', done => {
     chai
       .request(server)
       .post('/login')
-      .send({username: 10, email: 'testEmail2@gmail.com', rating: '5.0', verified: TRUE})
+      .send({username: 10, email: 'testEmail2@gmail.com', rating: 6.0, verified: true})
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.message).to.equals('Invalid input');
