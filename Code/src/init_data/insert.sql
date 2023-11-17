41fa0f60e4f6f98b5e84957c421662a414be57da
Miles Zheng <mzheng.012@gmail.com>
2023-11-16 17:01:54 -0700

    inserting email for requester now

--- Code/src/init_data/insert.sql
+++ Code/src/init_data/insert.sql
@@ -3,5 +3,5 @@ INSERT INTO users (username, email, password, rating, verified) VALUES
 ('Alice', 'alice@gmail.com', 'alicep', 5.0, FALSE);
 
 INSERT INTO jobs (name, job_id, description, requester, minPrice, maxPrice ) VALUES
-('Paint my house', 1, 'please paint my walls', 'Bob', 1.0, 100.0),
-('Mow my lawn', 2, 'please mow my lawn', 'Alice', 9.5, 10.0);
+('Paint my house', 1, 'please paint my walls', 'bob@gmail.com', 1.0, 100.0),
+('Mow my lawn', 2, 'please mow my lawn', 'alice@gmail.com', 9.5, 10.0);
