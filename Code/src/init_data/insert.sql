f16b800294c64c5ff86703810afa2cc62987a7a5
Miles Zheng <mzheng.012@gmail.com>
2023-12-04 14:05:47 -0700

    added email to posted jobs,fixed insert

--- Code/src/init_data/insert.sql
+++ Code/src/init_data/insert.sql
@@ -1,7 +1,7 @@
---INSERT INTO users (username, email, password, rating, verified) VALUES
---('Bob', 'bob@gmail.com', 'bobp', 10.0, TRUE),
+INSERT INTO users (username, email, password, rating, verified) VALUES
+('Bob', 'bob@gmail.com', 'bobp', 10.0, TRUE)
 --('Alice', 'alice@gmail.com', 'alicep', 5.0, FALSE);
 
---INSERT INTO jobs (name, job_id, description, requester, minPrice, maxPrice ) VALUES
---('Paint my house', 1, 'please paint my walls', 'Bob', 1.0, 100.0),
+INSERT INTO jobs (name, job_id, description, requester, email, minPrice, maxPrice ) VALUES
+('Paint my house', 1, 'please paint my walls', 'Bob', 'bob@gmail.com', 1.0, 100.0)
 --('Mow my lawn', 2, 'please mow my lawn', 'alice@gmail.com', 9.5, 10.0);
